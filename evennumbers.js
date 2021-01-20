02725087a47e30714b8481bfed51e5ce84925194
ishwar1510 <ijh.hkr9@gmail>
2021-01-20 21:48:51 +0530

    even and odd numbers

---
+++ evennumbers.js
@@ -0,0 +1,26 @@
+/*---print 1 to 10 even numbers using while loop ---*/
+
+
+// var x = 1;
+// while (x !==11 ){
+//   if(x % 2 == 0)
+//   console.log(x);
+//   x++;
+//
+// }
+
+
+// var i = 1;
+// while (i !==11 && i % 2 == 0){
+//   console.log(i)
+//   i++
+// }
+
+
+/*---print 1 to 10 ODD numbers using while loop ---*/
+
+var a = 1;
+while (a<=10){
+  console.log(a)
+  a += 2;
+}
