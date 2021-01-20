106048984fc1dc31b5267a42a932fa1ae866e8b2
ishwar1510 <ijh.hkr9@gmail>
2021-01-20 20:59:57 +0530

    while loop example

--- whileloop.js
+++ whileloop.js
@@ -1,5 +1,15 @@
-let i = 1;
-while (i<=10){
-  console.log(i);
-  i++;
+/*---write a program to print 1 to 10 numbers using while loop in javascript---*/
+
+// let i = 1;
+// while (i<=10){
+//   console.log(i);
+//   i++;
+// }
+
+/*----write a program to print 10 to 1 numbers using while loop in javascript--*/
+
+let x = 10;
+while (x>=1){
+  console.log(x);
+  x--;
 }
