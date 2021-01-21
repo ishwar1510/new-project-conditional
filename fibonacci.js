582a7fef3e23bea67a6cfef1e6321b2509df48af
ishwar1510 <ijh.hkr9@gmail>
2021-01-21 20:44:49 +0530

    print fibonacci numbers

---
+++ fibonacci.js
@@ -0,0 +1,33 @@
+
+/*----to print the fibonacci number series up to the given number--*/
+
+// const number = 17;
+// let n1 = 0, n2 = 1, nextTerm;
+//
+// console.log('Fibonacci Series:');
+// for (let i = 1; i <= number; i++) {
+//   console.log(n1);
+//   nextTerm = n1 + n2;
+//   n1 = n2;
+//   n2 = nextTerm;
+// }
+
+/*---------------------------------------------------------------*/
+const number = 5;
+let n1 = 0, n2 = 1, nextTerm;
+
+console.log('Fibonacci Series:');
+console.log(n1); // print 0
+console.log(n2); // print 1
+
+nextTerm = n1 + n2;
+
+while (nextTerm <= number) {
+
+  // print the next term
+  console.log(nextTerm);
+
+  n1 = n2;
+  n2 = nextTerm;
+  nextTerm = n1 + n2;
+}
