31dcc4b22bed97b428433242086616a0fb17dd49
ishwar1510 <ijh.hkr9@gmail>
2021-01-21 09:53:53 +0530

    1st commit

---
+++ newtest.js
@@ -0,0 +1,22 @@
+let emp = [
+  {name:'narasimha',loc:'chennai'},
+  {name:'ramesh',loc:'bangalore'},
+  {name:'mahesh',loc:'tamilnadu'},
+  {name:'vijay',loc:'andrapradesh'},
+  {name:'teja',loc:'kerala'}
+
+];
+
+console.log(emp.map((x)=> x.loc !== 'kerala'));
+
+------------------------------------------------------------------
+
+1)expecting function
+2)function iterate array value
+3)return
+
+/*--array methods ---*/
+
+filter()
+map()
+forEach()
