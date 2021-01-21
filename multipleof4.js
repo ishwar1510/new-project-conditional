d5a581e1a73dc3d3a64d3441349dd1fd1669ccd8
ishwar1510 <ijh.hkr9@gmail>
2021-01-21 06:17:00 +0530

    multiple of a number

---
+++ multipleof4.js
@@ -0,0 +1,18 @@
+// for(var i = 0; i < 41; i += 4){
+//   if(i != 0)
+//     console.log(i + ' ')
+// }
+
+
+// var x = 4;
+// while (x<41){
+//   console.log(x)
+//   x+=4;
+// }
+
+
+var a = 4;
+while(a<17){
+  console.log(a)
+  a+=4;
+}
