8fb25e12aea00eeaea625d49696dfe169075e40d
ishwar1510 <ijh.hkr9@gmail>
2021-01-21 20:15:43 +0530

    print even numbers

---
+++ printevennumbers.js
@@ -0,0 +1,17 @@
+/*---to print even numbers in given array---*/
+
+
+// var x = [24,23,28,26,22];
+// for(var i=0;i<x.length;i++) {
+//   if (x[i] % 2 == 0){
+//     console.log(x[i] + " is even");
+//   }
+// }
+
+
+/*---print even numbers given range---*/
+for(let i=0;i<=10;i++){
+  if (i % 2 == 0){
+    console.log(i)
+  }
+}
