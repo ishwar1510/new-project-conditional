4fab1b1df76ce517b49c2baefe30d9ca72b563e4
ishwar1510 <ijh.hkr9@gmail>
2021-01-21 09:13:42 +0530

    multiple table of 24

---
+++ factorsofnumber.js
@@ -0,0 +1,18 @@
+// function calculate(24) {
+//   var str = "0";
+//   for (var i = 1; i <= 24; i++) {
+//     if (num % i == 0) {
+//       str += ',' + i;
+//     }
+//   }
+//
+// }
+// console.log(str)
+
+/*----factors of 24 ---*/
+
+for(let i=0; i<=24;i++){
+  if(24%i == 0){
+    console.log(i)
+  }
+}
